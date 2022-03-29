8644950f6bb070f2557a9390b5b2fe60a00c4380
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-03-29 17:25:20 +0900

    Implement Header

---
+++ src/components/layouts/Header/Header.tsx
@@ -0,0 +1,50 @@
+import { Link, styled } from '@nextui-org/react';
+import { Switch, Container, Row } from '@nextui-org/react';
+
+import { useScrollPosition } from 'hooks/use-scroll-position';
+import { SunIcon } from 'assets/SunIcon';
+import { MoonIcon } from 'assets/MoonIcon';
+import { NavBar } from 'components/blocks/NavBar';
+import { Logo } from 'assets/Logo';
+
+const Header = styled('header', {
+  position: 'sticky',
+  top: 0,
+  height: '4.75rem',
+  display: 'flex',
+  alignItems: 'center',
+});
+
+export const HeaderComponent: React.FC = () => {
+  const scrollPosition = useScrollPosition();
+
+  const blurStyle = {
+    backdropFilter: `saturate(180%) blur(10px)`,
+    boxShadow: `rgb(2 1 1 / 10%) 0px 5px 20px -5px`,
+  };
+
+  return (
+    <Header css={scrollPosition > 30 && blurStyle}>
+      <Container md display="flex" alignItems="center" wrap="nowrap">
+        <Row justify="flex-start" align="center">
+          <Link href="/">
+            <Logo />
+          </Link>
+        </Row>
+        <Row justify="center" align="center">
+          <NavBar />
+        </Row>
+        <Row justify="flex-end" align="center">
+          <Switch
+            checked={true}
+            size="lg"
+            shadow
+            color="warning"
+            iconOn={<SunIcon filled />}
+            iconOff={<MoonIcon filled />}
+          />
+        </Row>
+      </Container>
+    </Header>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 
+import { Header } from 'components/layouts/Header';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Header />
       <Component {...pageProps} />
     </NextUIProvider>
   );
